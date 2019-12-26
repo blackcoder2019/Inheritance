648e848d5c36bd5d006e5b2b594128058e2ca909
Md Ariful Islam <48733739+blackcoder2019@users.noreply.github.com>
2019-12-27 03:10:38 +0600

    Initial commit

---
+++ src/developer/arifkhan/Main.java
@@ -0,0 +1,12 @@
+package developer.arifkhan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OutLander outLander = new OutLander(36);
+        outLander.accerlerate(30);
+        outLander.accerlerate(20);
+
+
+    }
+}
